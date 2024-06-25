c0885dbede68f61ec68e58494cc10a7d32d4761c
st-nhanho <hodangnhan24@gmail.com>
2024-06-25 22:45:47 +0700

    Update db config

--- app/database.py
+++ app/database.py
@@ -7,14 +7,11 @@ from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from .configs import settings
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/"
-    f"{settings.DB_NAME}?client_encoding=utf8"
+    f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, echo=False)
-SessionLocal = scoped_session(
-    sessionmaker(bind=engine, autocommit=False, autoflush=False)
-)
+SessionLocal = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
 
 
 # Dependency
