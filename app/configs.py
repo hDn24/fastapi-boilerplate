4dca0e8a5baf9e0f0799ea6b9a3d9d34cc90c1a5
st-nhanho <hodangnhan24@gmail.com>
2024-06-25 22:44:59 +0700

    Update settings config

--- app/configs.py
+++ app/configs.py
@@ -1,17 +1,22 @@
+import secrets
+
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = Field(default="0.0.0.0")
+    DB_HOST: str = Field(default="localhost")
     DB_PORT: int = Field(default=5432)
-    DB_NAME: str = Field(default="db")
+    DB_NAME: str = Field(default="postgres")
     DB_USER: str = Field(default="postgres")
     DB_PASS: str = Field(default="postgres")
+    API_V1_STR: str = Field(default="/api/v1")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=60 * 24 * 8)
+    SECRET_KEY: str = Field(default=secrets.token_urlsafe(32))
 
     class Config:
         env_file = ".env"
-        allow_mutation = False
+        # allow_mutation = False
 
 
 settings = Settings()
