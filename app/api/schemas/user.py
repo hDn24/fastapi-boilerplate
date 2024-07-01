dad4387e493bf99212a8ce9337ddc09e91df0f56
st-nhanho <hodangnhan24@gmail.com>
2024-07-01 09:29:24 +0700

    Add user schema

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -4,10 +4,23 @@ from pydantic import BaseModel, Field
 class UserBase(BaseModel):
     username: str = Field(example="hDn24")
     email: str = Field(example="hDn24@gmail")
-    password: str = Field(example="2ES69y0EFiPynrhAelkcmBTgOGTTirDBJ4G0R-y9qcQ")
     is_active: bool = Field(default=True)
     is_superuser: bool = Field(default=False)
 
+    class Config:
+        orm_mode = True
+
 
 class UseCreate(UserBase):
-    pass
+    password: str = Field(example="...")
+
+
+class User(UserBase):
+    hash_password: str = Field(examples="...")
+
+    class Config:
+        orm_mode = True
+
+
+class UserOut(UserBase):
+    id: int
