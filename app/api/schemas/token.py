7734ea2176621a44c983f22f57a45f979c583cc5
st-nhanho <hodangnhan24@gmail.com>
2024-07-01 09:20:31 +0700

    Add TokenPayload schema

--- app/api/schemas/token.py
+++ app/api/schemas/token.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel, Field
 class Token(BaseModel):
     access_token: str = Field(example="ey...")
     token_type: str = Field(default="bearer")
+
+
+class TokenPayload(BaseModel):
+    sub: int = Field(example=1)
