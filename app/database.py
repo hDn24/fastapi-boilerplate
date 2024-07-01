b6ffd31d3f0ba92b52edfd98c6dd91983acaa2d6
st-nhanho <hodangnhan24@gmail.com>
2024-07-01 14:44:47 +0700

    Remove duplicate code

--- app/database.py
+++ app/database.py
@@ -6,17 +6,22 @@ from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
 from .configs import settings
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-)
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
 
 
 # Dependency
 def get_db() -> Iterator[Session]:
+    """
+    Get a database session for executing database operations.
+
+    Returns:
+        Iterator[Session]: A context manager that provides a database session.
+            The session is automatically closed when the context is exited.
+    """
     db = SessionLocal()
     try:
         yield db
