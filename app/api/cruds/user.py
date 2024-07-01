f6577f7afac2eeb9acdf2a4749b9b3e52720c846
st-nhanho <hodangnhan24@gmail.com>
2024-07-01 21:22:42 +0700

    Refactor code

--- app/api/cruds/user.py
+++ app/api/cruds/user.py
@@ -1,16 +1,25 @@
 from sqlalchemy.orm import Session
 
 from app.api.models.user import User
-from app.api.schemas import user as schemas
 from app.api.schemas.user import UseCreate
 from app.api.utils.security import get_password_hash
 
 
-def create_user(db: Session, user_create: UseCreate) -> User:
-    user_dict = user_create.dict()
-    user_dict["hash_password"] = get_password_hash(user_create.password)
-    user_hashed = schemas.User(**user_dict)
-    user = User(**user_hashed.dict())
+def create_user(db: Session, user_data: UseCreate) -> User:
+    """
+    Create a new user in the database.
+
+    Args:
+        db: The database session.
+        user_data: The data of the user to be created.
+
+    Returns:
+        User: The created user object.
+    """
+    # Generate the hashed password from the provided password
+    hashed_password = get_password_hash(user_data.password)
+    # Create the user object and add it to the database in one step
+    user = User(**user_data.dict(exclude={"password"}), hash_password=hashed_password)
     db.add(user)
     db.commit()
     db.refresh(user)
--- app/api/routers/login.py
+++ app/api/routers/login.py
@@ -21,20 +21,45 @@ def create_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Session = Depends(get_db),
 ) -> Token:
+    """
+    Create an access token for the user.
+
+    Args:
+        form_data: The form data containing the user's username and password.
+        db: The database session. Defaults to the session obtained from the `get_db` dependency.
+
+    Returns:
+        The access token for the user.
+
+    Raises:
+        HTTPException: If the username or password is incorrect or the user is inactive.
+    """
+    # Authenticate the user
     user = crud.authenticate(db, form_data.username, form_data.password)
 
+    # Check if the user exists and is active
     if not user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     elif not user.is_active:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User is not active")
 
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    return Token(access_token=security.create_access_token(user.id, expires_delta=access_token_expires))
+    # Create the access token with the user's ID and expiration time
+    access_token_expiration = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(user.id, expires_delta=access_token_expiration)
+
+    # Return the access token as a response
+    return Token(access_token=access_token)
 
 
 @router.post("/test-token", response_model=UserOut)
 def test_token(current_user: CurrentUser) -> Any:
     """
-    Test access token
+    This endpoint is used to test the validity of the access token by returning the current user object.
+
+    Args:
+        current_user: The current user object obtained from the dependency.
+
+    Returns:
+        Any: The current user object.
     """
     return current_user
