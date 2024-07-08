1d1dccddb67e21ce83b8ab2952287c6aa48da6ce
st-nhanho <hodangnhan24@gmail.com>
2024-07-08 09:01:35 +0700

    Change email to unique in user table

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -8,7 +8,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
-    email = Column(String, index=True)
+    email = Column(String, index=True, unique=True)
     hash_password = Column(String)
     is_active = Column(Boolean)
     is_superuser = Column(Boolean)
