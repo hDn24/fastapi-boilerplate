3ed2b53740180dac0c987cead6441d4b3d7d600d
hDn24 <hodangnhan24@gmail.com>
2025-03-21 09:50:50 +0700

    🐛 fix: fix authentication

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.api.routers.login import router as login_router
 from app.api.routers.user import router as user_router
 from app.configs import settings
 
-app = FastAPI(title=settings.PROJECT_NAME, root_path=settings.API_V1_STR)
+app = FastAPI(title=settings.PROJECT_NAME)
 
 
 app.include_router(login_router, prefix=settings.API_V1_STR)
